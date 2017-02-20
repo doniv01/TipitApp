169deb80574fc53ddc847174f575c92265e514b3
Vinod <vkrajpurohit@paypal.com>
2017-02-20 11:21:58 +0800

    Initial Commit

---
+++ tipit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipit
+//
+//  Created by Rajpurohit, Vinod on 20/2/17.
+//  Copyright © 2017 Vinod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
