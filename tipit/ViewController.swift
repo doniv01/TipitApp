a80a775adeb1868fb8eecc4e5ae434117d831393
Vinod <vkrajpurohit@paypal.com>
2017-02-21 04:51:36 +0800

    Final compiled version

--- tipit/ViewController.swift
+++ tipit/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func onTap(_ sender: Any) {
+  
+        view.endEditing(true)
+        
+    }
+  
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentage = [0.18, 0.2, 0.30]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
